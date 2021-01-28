55f071fce04180709476299347f2c1d6500802bd
Vladyslav <69809932+Mr2NEC@users.noreply.github.com>
2021-01-28 03:44:35 +0200

    commit

--- src/component/ConfirmationModal.js
+++ src/component/ConfirmationModal.js
@@ -2,28 +2,27 @@
 import React, {useState} from 'react';
 import { Button,Modal } from 'react-bootstrap';
 
-export default function ConfirmationModal({id, login}) {
+export default function ConfirmationModal({connectionId, login, status}) {
     const [show, setShow] = useState(false);
   
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleStatus = () => setShow(status);
   
     return (
       <>
-        <Button variant="success" onClick={handleShow}>
+        <Button variant="success" onClick={handleStatus}>
             Play
         </Button>
   
-        <Modal show={show} onHide={handleClose}>
+        <Modal show={show} onHide={handleStatus}>
           <Modal.Header closeButton>
             <Modal.Title>The {login} invites you to join the game.</Modal.Title>
           </Modal.Header>
           <Modal.Body>Do you want to join the game?</Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
+            <Button variant="secondary" onClick={console.log('no')}>
               No
             </Button>
-            <Button variant="primary" onClick={()=>{ console.log(id);handleClose()}}>
+            <Button variant="primary" onClick={()=>{console.log(connectionId)}}>
               Yes
             </Button>
           </Modal.Footer>
